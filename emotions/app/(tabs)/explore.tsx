2889d9f61ce36481e260441b088b2f2af1fe2c9d
bhanupratapsingh21 <bhanupss137@gmail.com>
2025-03-19 18:47:11 +0530

    Done with the Init

---
+++ emotions/app/(tabs)/explore.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-red-700 text-lg">Hello ji</Text>
+    </View>
+  );
+}
